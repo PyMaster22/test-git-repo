f5947895a6a75bec270f340e1655a85165a2f497
PyMaster22 <59183571+PyMaster22@users.noreply.github.com>
2021-12-01 10:27:12 -0800

    Update genrsakey.py

--- python/genrsakey.py
+++ python/genrsakey.py
@@ -1,6 +1,7 @@
 import rsa
-(pubkey,privkey)=rsa.newkeys(3072)
-privatekey=privkey.save_pkcs1().decode("utf8")
-publickey=pubkey.save_pkcs1().decode("utf8")
+def pemkeys(size=3072):
+	(pubkey,privkey)=rsa.newkeys(size)
+	return(privkey.save_pkcs1().decode("utf8"),pubkey.save_pkcs1().decode("utf8"))
 if(__name__=="__main__"):
-	print(privatekey,publickey,sep="\n")
+	keys=pemkeys()
+	print(keys[0],keys[1],sep="\n")
